0c32bc429725342660ddf85763b9c57c5bb66b10
alamin-wd <mdalamin67198@gmail.com>
2024-05-22 21:31:55 +0600

    Toast Added

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const BookDetails = () => {
 
@@ -10,6 +12,18 @@ const BookDetails = () => {
     const book = books.find(book => book.bookId === idInt);
 
     const { book_name, author, image, category, rating, tags, review, totalPages, publisher, year_of_publishing } = book;
+
+// Read Btn Handler
+    const handleRead = () => {
+
+        toast('You Have Successfully Added The Book As Read!');
+    }
+
+    // Wishlist Btn Handler
+    const wishList = () => {
+
+        toast('You Have Successfully Added The Book to Wishlist!');
+    }
 
 
     return (
@@ -24,17 +38,11 @@ const BookDetails = () => {
             {/* Right Side */}
             <div className="md:w-3/5">
                 <h2 className="text-[#131313] text-2xl md:text-4xl font-bold">{book_name}</h2>
-
                 <h4 className="text-[#131313CC] font-medium my-4">By: {author}</h4>
-
                 <hr />
-
                 <h3 className="text-[#131313CC] my-2 font-medium">{category}</h3>
-
                 <hr />
-
                 <p className="my-4"><span className="text-[#131313] font-bold">Review:</span> {review}</p>
-
                 <div className="flex items-center justify-center md:justify-start gap-4 my-4">
                     <span className="text-[#131313] font-bold">Tags:</span>
 
@@ -42,9 +50,7 @@ const BookDetails = () => {
 
                     <span className="bg-[#23BE0A0D] text-[#23BE0A] font-medium px-3 py-1 rounded-full">{tags[1]}</span>
                 </div>
-
                 <hr />
-
                 <div className="md:w-1/2">
                     <table className="table-md">
                         <tbody>
@@ -76,13 +82,20 @@ const BookDetails = () => {
 
                 {/* Buttons */}
                 <div className="flex gap-6 mt-2 justify-center md:justify-start">
-                    <button className=" bg-white border-2 border-[#59C6D2] hover:bg-[#59C6D2] px-2 py-1 md:px-5 md:py-2 rounded-md md:rounded-lg text-black hover:text-white md:text-lg font-medium md:font-semibold">
+                    <button onClick={handleRead}
+                        className=" bg-white border-2 border-[#59C6D2] hover:bg-[#59C6D2] px-2 py-1 md:px-5 md:py-2 rounded-md md:rounded-lg text-black hover:text-white md:text-lg font-medium md:font-semibold">
                         Read
                     </button>
 
-                    <button className=" bg-[#50B1C9] hover:bg-white hover:border-2 hover:border-[#50B1C9] hover:text-black px-2 py-1 md:px-4 md:py-2 rounded-md md:rounded-lg text-white md:text-lg font-medium md:font-semibold">
+                    <button onClick={wishList}
+                    className=" bg-[#50B1C9] hover:bg-white hover:border-2 hover:border-[#50B1C9] hover:text-black px-2 py-1 md:px-4 md:py-2 rounded-md md:rounded-lg text-white md:text-lg font-medium md:font-semibold">
                         Wishlist
                     </button>
+                </div>
+
+                {/* Toast */}
+                <div>
+                    <ToastContainer />
                 </div>
 
             </div>
